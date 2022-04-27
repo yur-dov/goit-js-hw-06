3c1a6f3d8df6e4722b217f6fdd6947f0f8e81e26
Yurii <Rapacta@gmail.com>
2022-04-28 00:50:34 +0200

    task-9-complite

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+const refMain = {
+  body: document.body,
+  btn: document.querySelector('.change-color'),
+  span: document.querySelector('.color')
+}
+
+const foo = event => {
+  const colorRandom = getRandomHexColor();
+  refMain.span.textContent = colorRandom;
+  refMain.body.style.backgroundColor = colorRandom;
+}
+
+refMain.btn.addEventListener('click', foo);
