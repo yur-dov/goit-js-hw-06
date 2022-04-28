60398fbe9b3b14f96fee37724f6b5b2f5f287df5
Yurii <Rapacta@gmail.com>
2022-04-28 22:59:52 +0200

    task-8-complite

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+'use strict'
 
+const inputForm = document.querySelector('.login-form')
+inputForm.addEventListener('submit', submitForm)
+
+function checkInput (event) {
+    const inputMail = event.currentTarget.email;
+    const inputPasswors = event.currentTarget.password;
+    
+    if (inputMail.value === '' || inputPasswors.value === '') {
+        return alert('Each field must be filled!')
+    }
+    const formData = new FormData(event.currentTarget)
+    const objForm = {}
+    objForm.email = event.currentTarget.email.value;
+    objForm.password = event.currentTarget.password.value;
+    console.log(objForm);
+    event.currentTarget.reset();
+}
+
+    function submitForm (event) {
+        event.preventDefault()
+        checkInput(event)
+    }
