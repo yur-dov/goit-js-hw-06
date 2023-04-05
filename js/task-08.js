3c9fed56d002ec32260f688a488ed0b636d9b9f6
Yurii <89393975+yur-dov@users.noreply.github.com>
2023-04-05 14:19:16 -0600

    re-create

--- js/task-08.js
+++ js/task-08.js
@@ -6,15 +6,19 @@ inputForm.addEventListener('submit', submitForm)
 function checkInput (event) {
     const inputMail = event.currentTarget.email;
     const inputPasswors = event.currentTarget.password;
-    
+    const arrForm = [];
+
     if (inputMail.value === '' || inputPasswors.value === '') {
         return alert('Each field must be filled!')
     }
     const formData = new FormData(event.currentTarget)
     const objForm = {}
+    
     objForm.email = event.currentTarget.email.value;
     objForm.password = event.currentTarget.password.value;
     console.log(objForm);
+    arrForm.push(objForm);
+    console.log(arrForm);
     event.currentTarget.reset();
 }
 
